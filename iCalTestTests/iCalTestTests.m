edab6d4fbc01aa42eeb3dd5af4810ed8b821275e
Artwalk <frank.holy.art@gmail.com>
2014-04-30 01:01:08 +0800

    Squashed '=iCal_iOS_Test/' content from commit 00071c0
    git-subtree-dir: =iCal_iOS_Test
    git-subtree-split: 00071c0b74b5b805ab5ed4a65796f614d04a59f2

---
+++ iCalTestTests/iCalTestTests.m
@@ -0,0 +1,34 @@
+//
+//  iCalTestTests.m
+//  iCalTestTests
+//
+//  Created by artwalk on 4/29/14.
+//  Copyright (c) 2014 artwalk. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iCalTestTests : XCTestCase
+
+@end
+
+@implementation iCalTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
